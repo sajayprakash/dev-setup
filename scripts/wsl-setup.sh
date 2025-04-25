0f3cdc419c75fa1d129edb9abd1fa5027c6d1220
Sajay Prakash <sajayprakashk@gmail.com>
2025-04-25 13:39:53 +0530

    add zsh and nvm

--- scripts/wsl-setup.sh
+++ scripts/wsl-setup.sh
@@ -1,21 +1,42 @@
 #!/bin/bash
 
-# Update ubuntu
+# Update system
 sudo apt update && sudo apt upgrade -y
 
-# Install dependencies
-sudo apt install -y curl unzip zsh git build-essential
+# Install packages
+sudo apt install curl unzip zsh build-essential -y
 
-# Install fast-node-manager(fnm)
-curl -fsSL https://fnm.vercel.app/install | bash
+# ZSH and Ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+chsh -s $(which zsh)
+exec zsh
+
+# ZSH Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+# Install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install --lts
 
 # Install pnpm
-curl -fsSL https://get.pnpm.io/install.sh | sh
+curl -fsSL https://get.pnpm.io/install.sh | sh -
 
 # Install bun
 curl -fsSL https://bun.sh/install | bash
 
-# Install github cli
+# Install rustup
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Setup git
+git config --global user.email "sajayprakashk@gmail.com"
+git config --global user.name "Sajay Prakash"
+git config --global init.defaultBranch "main"
+
+# Install gh
 (type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
 	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
         && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
@@ -25,9 +46,4 @@ curl -fsSL https://bun.sh/install | bash
 	&& sudo apt update \
 	&& sudo apt install gh -y
 
-# Setup git
-git config --global user.email "sajayprakashk@gmail.com"
-git config --global user.name "Sajay Prakash"
-git config --global init.defaultBranch main
-
-echo "Setup complete!"
+gh auth login -s delete_repo
